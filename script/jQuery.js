cb64ac912b040b8ad6b0a50f136fbb092dc0787a
Ehsanakr <84423517+Ehsanakr@users.noreply.github.com>
2023-12-15 02:51:30 +0800

    touch

--- script/jQuery.js
+++ script/jQuery.js
@@ -1,43 +1,41 @@
-function handleScrollDown(elementSelector, sectionId) {
-  function scrollDownFunction() {
-    window.scrollBy({
-      top: window.innerHeight,
-    });
-  }
 
-  function scrollToTopFunction() {
-    window.scrollTo({
-      top: 0,
-    });
-  }
 
-  function setEventListeners(scrollButton, scrollFunction) {
-    scrollButton.addEventListener("click", scrollFunction);
-    scrollButton.addEventListener("touchstart", scrollFunction);
-  }
-
+// ScrollDown rotation on scroll
+function handlScrollDown(elementSelector, sectionId) {
   window.addEventListener("scroll", () => {
-    const scrollPosition = window.scrollY ;
     const element = document.querySelector(elementSelector);
     const lastSection = document.getElementById(sectionId);
-    var scrollButton = document.getElementById("scrollDown");
 
-    if (scrollPosition <= lastSection.offsetTop) {
+    if ( window.scrollY <= lastSection.offsetTop) {
       element.style.transform = `scaleY(1)`;
-      setEventListeners(scrollButton, scrollDownFunction);
+
+      element.addEventListener("click", function () {
+        window.scrollTo({
+          top: window.scrollY + window.innerHeight,
+        });
+      });
+      element.addEventListener("touchstart", function () {
+        window.scrollTo({
+          top: window.scrollY + window.innerHeight,
+        });
+      });
     } else {
       element.style.transform = `scaleY(-1)`;
-      setEventListeners(scrollButton, scrollToTopFunction);
+      element.addEventListener("click", function () {
+        window.scrollTo({
+          top: 0,
+        });
+      });
+      element.addEventListener("touchstart", function () {
+        window.scrollTo({
+          top: 0,
+        });
+      });
     }
   });
-
 }
-
-// button element selector, "last section" element ID
-handleScrollDown(".scrollDown", "reachUs");
-
-
-
+// Element selector , last section ID selector
+handlScrollDown(".scrollDown", "reachUs");
 
 
 
@@ -74,7 +72,7 @@ function handleScrollVisibility(
 
 handleScrollVisibility(".captionMain", window.innerHeight * 0.2, 0.0);
 
-// Smooth scroll to sections on click (both menu and buttons)
+// Smooth SCROLL TO sections on click (both menu and buttons)
 $(document).on("click", 'a[href^="#"], button[href^="#"]', function (event) {
   event.preventDefault();
   smoothScroll($(event.target).attr("href"));
@@ -108,9 +106,226 @@ function activateMenuItem() {
 activateMenuItem();
 $(window).on("scroll", activateMenuItem);
 
-// Set first menu item as current by default
-function setup() {
-  $(".menu__item").first().addClass("menu__item--current");
+
+
+//***********     text gsap     ***********//
+
+function updateScrollProgress() {
+  const maxScroll = document.documentElement.scrollHeight - window.innerHeight;
+  const currentScroll = window.scrollY;
+  scrollProgress = currentScroll / maxScroll;
 }
 
-$(document).ready(setup);
+// Set up content scroll triggers
+function setupContentScrollTrigger() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const content = [
+    {
+      list_top: "",
+      title: "",
+      subtitle: "Bilateral OTC Derivatives with Intent-Based execution.<br/>Leverage trade any asset permissionlessly with hyper-efficient liquidity.",
+      titleSYMM: "SYMMIO",
+      list_top: "",
+      selector: ".section.one",
+    },
+    {
+      titleSYMM: "",
+      title: "",
+      subtitle: "",
+      list_top:
+        "<span class= head-span>HOW TO TRADE DERIVATIVES<br>with INTENTs using SYMMIO.</span> " +
+        "</br></br>" +
+        " <span>• FORMULATE your INTENT via 3rd party frontend</span></br>" +
+        " <span>• SEND your INTENT to the pool</span></br>" +
+        " <span>• Counterparties (Hedgers) SEE your INTENT in the pool</span></br>" +
+        " <span>• Counterparty (Hedger) CLAIMs your intent</span></br>" +
+        " <span>• Claimed INTENTs CREATE a trade</span></br>" +
+        " <span>• Both parties LOCK collateral</span>",
+
+      selector: ".section.two",
+    },
+    {
+      titleSYMM: "",
+      title: "",
+      subtitle: "",
+      list_top:
+        "<span class= head-span>HOW TO TRADE DERIVATIVES<br>with INTENTs using SYMMIO.</span> " +
+        "</br></br>" +
+        " <span class=active-span>• FORMULATE your INTENT via 3rd party frontend</span></br>" +
+        " <span>• SEND your INTENT to the pool</span></br>" +
+        " <span>• Counterparties (Hedgers) SEE your INTENT in the pool</span></br>" +
+        " <span>• Counterparty (Hedger) CLAIMs your intent</span></br>" +
+        " <span>• Claimed INTENTs CREATE a trade</span></br>" +
+        " <span>• Both parties LOCK collateral</span>",
+
+      selector: ".section.three",
+    },
+    {
+      titleSYMM: "",
+      title: "",
+      subtitle: "",
+      list_top:
+        "<span class= head-span>HOW TO TRADE DERIVATIVES<br>with INTENTs using SYMMIO.</span> " +
+        "</br></br>" +
+        " <span>• FORMULATE your INTENT via 3rd party frontend</span></br>" +
+        " <span class=active-span>• SEND your INTENT to the pool</span></br>" +
+        " <span>• Counterparties (Hedgers) SEE your INTENT in the pool</span></br>" +
+        " <span>• Counterparty (Hedger) CLAIMs your intent</span></br>" +
+        " <span>• Claimed INTENTs CREATE a trade</span></br>" +
+        " <span>• Both parties LOCK collateral</span>",
+
+      selector: ".section.four",
+    },
+    {
+      titleSYMM: "",
+      title: "",
+      subtitle: "",
+      list_top:
+        "<span class= head-span>HOW TO TRADE DERIVATIVES<br>with INTENTs using SYMMIO.</span> " +
+        "</br></br>" +
+        " <span>• FORMULATE your INTENT via 3rd party frontend</span></br>" +
+        " <span>• SEND your INTENT to the pool</span></br>" +
+        " <span class=active-span>• Counterparties (Hedgers) SEE your INTENT in the pool</span></br>" +
+        " <span>• Counterparty (Hedger) CLAIMs your intent</span></br>" +
+        " <span>• Claimed INTENTs CREATE a trade</span></br>" +
+        " <span>• Both parties LOCK collateral</span>",
+
+      selector: ".section.five",
+    },
+    {
+      titleSYMM: "",
+      title: "",
+      subtitle: "",
+      list_top:
+        "<span class= head-span>HOW TO TRADE DERIVATIVES<br>with INTENTs using SYMMIO.</span> " +
+        "</br></br>" +
+        " <span>• FORMULATE your INTENT via 3rd party frontend</span></br>" +
+        " <span>• SEND your INTENT to the pool</span></br>" +
+        " <span>• Counterparties (Hedgers) SEE your INTENT in the pool</span></br>" +
+        " <span class=active-span>• Counterparty (Hedger) CLAIMs your intent</span></br>" +
+        " <span>• Claimed INTENTs CREATE a trade</span></br>" +
+        " <span>• Both parties LOCK collateral</span>",
+
+      selector: ".section.six",
+    },
+    {
+      titleSYMM: "",
+      title: "",
+      subtitle: "",
+      list_top:
+        "<span class= head-span>HOW TO TRADE DERIVATIVES<br>with INTENTs using SYMMIO.</span> " +
+        "</br></br>" +
+        " <span>• FORMULATE your INTENT via 3rd party frontend</span></br>" +
+        " <span>• SEND your INTENT to the pool</span></br>" +
+        " <span>• Counterparties (Hedgers) SEE your INTENT in the pool</span></br>" +
+        " <span>• Counterparty (Hedger) CLAIMs your intent</span></br>" +
+        " <span class=active-span>• Claimed INTENTs CREATE a trade</span></br>" +
+        " <span>• Both parties LOCK collateral</span>",
+
+      selector: ".section.seven",
+    },
+    {
+      titleSYMM: "",
+      title: "",
+      subtitle: "",
+      list_top:
+        "<span class= head-span>HOW TO TRADE DERIVATIVES<br>with INTENTs using SYMMIO.</span> " +
+        "</br></br>" +
+        " <span>• FORMULATE your INTENT via 3rd party frontend</span></br>" +
+        " <span>• SEND your INTENT to the pool</span></br>" +
+        " <span>• Counterparties (Hedgers) SEE your INTENT in the pool</span></br>" +
+        " <span>• Counterparty (Hedger) CLAIMs your intent</span></br>" +
+        " <span>• Claimed INTENTs CREATE a trade</span></br>" +
+        " <span class=active-span>• Both parties LOCK collateral</span>",
+
+      selector: ".section.eight",
+    },
+    {
+      titleSYMM: "",
+      list_top: "",
+      title: "",
+      subtitle: "",
+      list_top:
+        "<span class= head-span>Neutral Parties as Arbiters</span> " +
+        "</br></br>" +
+        " <span class=active-span>Arbiters are advanced liquidators ensuring all parties adhere to the rules and maintain solvency. </span></br>"+
+        " <span class=active-span>Both sides can be liquidated and their actions disputed, creating a trustless and highly capital efficient system.</span></br>",
+      selector: ".section.nine",
+    },
+    {
+      titleSYMM: "",
+      title: "",
+      subtitle: "",
+      list_top:
+        "<span class= head-span>PartyA and PartyB are SYMMETRICAL</span> " +
+        "</br></br>" +
+        " <span class=active-span>One side LONGs 1 BTC.</span></br>" +
+        " <span class=active-span>The other side SHORTs 1 BTC.</span></br>" +
+        " <span class=active-span>The loss of one side is the win of the other.</span></br>" ,
+      selector: ".section.ten",
+    },
+    {
+      titleSYMM: "",
+      title: "",
+      subtitle: "",
+      list_top: "",
+      selector: ".section.eleven",
+    },
+  ];
+
+  content.forEach((item, i, arr) => {
+    const onUpdate = function () {
+      const time = this.time();
+      const duration = this.duration();
+
+      if (time >= duration || time <= 0) {
+        return;
+      }
+    };
+    // Create a timeline for the intro animation
+    const introTimeline = gsap.timeline();
+
+    // Add fade-in animation for the titles, subtitles, and other elements
+    introTimeline.from(
+      `${item.selector} .box__text`,
+      { opacity: 0, duration: 5 },
+      0
+    );
+
+    const timeline = new gsap.timeline({
+      scrollTrigger: {
+        trigger: item.selector,
+        scrub: true,
+        start: "top 75%",
+        end: `bottom ${i < arr.length - 1 ? "75%" : "bottom"}`,
+      },
+    })
+      .to(
+        `${item.selector} .title`,
+        { text: `${item.title}`, ease: "linear", duration: 0.125, onUpdate },
+        0
+      )
+      .to(
+        `${item.selector} .titleSYMM`,
+        { text: `${item.titleSYMM}`, ease: "linear", duration: 0.0, onUpdate },
+        0
+      )
+      .to(
+        `${item.selector} .subtitle`,
+        { text: `${item.subtitle}`, ease: "linear", duration: 0.0, onUpdate },
+        0
+      )
+      .to(
+        `${item.selector} .list_top`,
+        { text: `${item.list_top}`, ease: "linear", duration: 0, onUpdate },
+        0
+      );
+
+    if (i < arr.length - 1) {
+      timeline.yoyo(true).repeat(1).repeatDelay(0.5);
+    }
+  });
+}
+
+
